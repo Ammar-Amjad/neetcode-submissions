f30f4b27a0c8c18a5a0161dbd97b20e3ac862ab2
Ammar Amjad <39439739+Ammar-Amjad@users.noreply.github.com>
2026-07-08 18:18:17 -0500

    Add: squares-of-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-0.py
@@ -0,0 +1,17 @@
+from collections import deque
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+
+        l, r = 0, len(nums) - 1
+        
+        res = deque()
+
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                res.appendleft(nums[l]**2)
+                l += 1
+            else:
+                res.appendleft(nums[r]**2)
+                r -= 1
+                
+        return list(res) 
